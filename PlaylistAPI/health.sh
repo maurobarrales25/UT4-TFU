817c3954e21c3a97bb71069d7702e11aed7407dd
NicooGon <nicolasd.gonzalez@correo.ucu.edu.uy>
2025-11-02 20:53:23 -0300

    playlist health and script update

--- PlaylistAPI/health.sh
+++ PlaylistAPI/health.sh
@@ -16,10 +16,24 @@ echo " Probando endpoint con DB caida."
 sleep 2
 curl -X GET http://localhost:8020/playlistapi/playlist/health
 echo ""
-
-# Reanudar DB
 docker start mongodb
 echo ""
+
+# Probar endpoint con microservicio caido.
+echo " Probando endpoint con UserAPI caida."
+sleep 2
+docker stop user-api
+curl -X GET http://localhost:8020/playlistapi/playlist/health
+echo ""
+docker start user-api
+
+# Probar endpoint con microservicio con DB caida.
+echo " Probando endpoint con UserAPI con DB caida."
+sleep 2
+docker stop user-db
+curl -X GET http://localhost:8020/playlistapi/playlist/health
+echo ""
+docker start user-db
 
 echo "Fin de la prueba del Health Endpoint. Presiona ENTER para cerrar."
 read
