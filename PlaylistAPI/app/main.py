8d266ef368c54f4956de038afcc889cf09afa387
Mauro <bmauro25@gmaile.com>
2025-11-16 22:46:31 -0300

    ut5 metrics

--- PlaylistAPI/app/main.py
+++ PlaylistAPI/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from controller import playlist_controller
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(root_path="/playlistapi")
 
 app.include_router(playlist_controller.router)
+
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
